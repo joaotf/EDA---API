fe610dead89aa3a60b4d480e215254fc21a85405
João Tomé <joaotf159@gmail.com>
2019-10-20 09:29:45 -0300

    Mudança YARN

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyparser = require('body-parser')
 const requireDir = require('require-dir')
-const porta = 8080;
+const porta = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json())
